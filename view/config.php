2661ebed7bc04b7a39e21458ddef44facf810af2
Pablo R <pablo.mg@hotmail.com>
2018-10-10 21:22:29 -0300

    teste view

--- view/config.php
+++ view/config.php
@@ -1,8 +1,8 @@
 <?php
-	define('DB_HOSTNAME', 'mysql');
-	define('DB_USERNAME', 'u681047708_aloca');
-	define('DB_PASSWORD', '497982');
-	define('DB_DATABASE','u681047708_tcc');
+	define('DB_HOSTNAME', 'localhost');
+	define('DB_USERNAME', 'root');
+	define('DB_PASSWORD', null);
+	define('DB_DATABASE','tcc');
 	define('DB_PREFIX','cw');
 	define('DB_CHARSET','utf8');
 ?>
